5ee59b7c13d3d30ea2c58d4fdda836b61615fe84
Bartosz Arseniuk <bartekarseniuk1701@gmail.com>
2024-08-22 20:20:53 +0200

    Create welcome page

---
+++ src/components/HomePage/HomePage.vue
@@ -0,0 +1,44 @@
+<template>
+<main>
+    <section id="welcome">
+        <WelcomePage />
+    </section>
+    <section id="our-goal">
+        <h1>Our Goal</h1>
+    </section>
+    <section id="history">
+        <h1>Cabbie history</h1>
+    </section>
+    <section id="offer">
+        <h1>Offer</h1>
+    </section>
+    <section id="check-offer">
+        <h1>Check Offer</h1>
+    </section>
+    <section id="lets-begin">
+        <h1>Lets begin</h1>
+    </section>
+    <section id="contact">
+        <h1>Contact</h1>
+    </section>
+</main>
+</template>
+
+    
+<script>
+import WelcomePage from './WelcomePage.vue';
+
+export default {
+    components: {
+        WelcomePage
+    }
+};
+</script>
+
+    
+<style>
+main section {
+    width: 100%;
+    margin: 0;
+}
+</style>
---
+++ src/components/HomePage/WelcomePage.vue
@@ -0,0 +1,93 @@
+<template>
+<div class="welcome-page">
+    <div class="logo">
+        <img src="@/assets/images/cabbie-logo.svg" alt="cabbie">
+    </div>
+    <p class="title">PARTNER KIEROWCÓW APLIKACJI TAXI</p>
+    <button class="lets-begin">POZNAJMY SIĘ</button>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'WelcomePage',
+};
+</script>
+
+<style scoped>
+.welcome-page {
+    background-image: url('@/assets/images/driver-dressed-elegant-costume 1.svg');
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: hidden;
+}
+
+.logo {
+    background-color: #FCB331;
+    height: 20vmin;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20vh;
+}
+
+.logo img {
+    max-height: 75%;
+    max-width: 75%;
+    height: auto;
+    width: auto;
+}
+
+.title {
+    color: #F0F0F0;
+    font-family: 'Roboto-Light', 'sans-serif';
+    font-size: 42px;
+    margin-top: 80px;
+    text-align: center;
+}
+
+.lets-begin {
+    background-color: transparent;
+    border: 2px solid #FCB331;
+    border-radius: 8px;
+    color: #FCB331;
+    cursor: pointer;
+    font-family: 'Roboto-Light', 'sans-serif';
+    font-size: 28px;
+    padding: 10px 20px;
+    transition: all 0.3s ease;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.lets-begin:hover {
+    color: #1E1E1E;
+    background-color: #FCB331;
+    border-color: #F0F0F0;
+    box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
+}
+
+@media (max-width: 768px) {
+    .logo {
+        height: 50vmin;
+        margin-top: 10vh;
+    }
+
+    .logo img {
+        max-height: 50%;
+        max-width: 50%;
+        height: auto;
+        width: auto;
+    }
+
+    .title {
+        font-size: 36px;
+        margin-top: 100px;
+    }
+}
+</style>
