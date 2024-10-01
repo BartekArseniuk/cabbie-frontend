da640580e2ddeb78db9f187663eda774d507517e
Wojciech Męczyński <wojteq898@gmail.com>
2024-10-01 22:28:36 +0200

    Add reviews

--- src/components/Views/AboutUs/OpinionsSection.vue
+++ src/components/Views/AboutUs/OpinionsSection.vue
@@ -1,28 +1,98 @@
 <template>
-    <div class="opinions">
-        <p class="title">CO MÓWIĄ O NAS KIEROWCY</p>
-        <p class="opinion">Will be soon</p>
+<div class="opinions">
+    <p class="title">CO MÓWIĄ O NAS KIEROWCY</p>
+    <div class="reviews-container">
+        <ReviewComponent v-for="(review, index) in reviews" :key="index" :reviewText="review.text" :authorName="review.author_name" />
     </div>
+    <p class="opinion" v-if="reviews.length === 0">Brak opinii do wyświetlenia</p>
+
+    <div class="footer">
+        <p class="footer-text">
+            Opinie dostarczone przez <img class="google-logo" src="@/assets/images/Google-Logo.wine.svg" alt="Google logo" />
+        </p>
+    </div>
+</div>
 </template>
 
+<script>
+import ReviewComponent from './ReviewComponent';
+
+export default {
+    components: {
+        ReviewComponent
+    },
+    data() {
+        return {
+            reviews: []
+        };
+    },
+    mounted() {
+        this.fetchReviews();
+    },
+    methods: {
+        async fetchReviews() {
+            try {
+                const response = await fetch(`http://localhost:8000/reviews/ChIJI7_8IzvHkwgRjBdfCt_u4cg`);
+                const data = await response.json();
+                this.reviews = data;
+            } catch (error) {
+                console.error('Error fetching reviews:', error);
+            }
+        }
+    }
+}
+</script>
+
 <style lang="scss" scoped>
-    .opinions {
-        height: 50vh;
-        display: flex;
-        align-items: center;
-        overflow: hidden;
-        flex-direction: column;
-    }
+.opinions {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 5vh;
+}
+
+.title {
+    color: $primary-color;
+    font-family: 'Roboto-Light', 'sans-serif';
+    font-size: 42px;
+    text-align: center;
+    margin-top: 100px;
+}
+
+.reviews-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+    align-items: center;
+}
+
+.opinion {
+    font-family: 'Roboto-Light', 'sans-serif';
+    color: $white;
+}
+
+.footer {
+    margin-top: 20px;
+    text-align: center;
+    padding: 0 50px;
+}
+
+.footer-text {
+    font-family: 'Roboto-Light', 'sans-serif';
+    font-size: 18px;
+    color: $white;
+}
+
+.google-logo {
+    height: 100px;
+    vertical-align: middle;
+}
+
+@media (max-width: 768px) {
     .title {
-        color: $primary-color;
-        font-family: 'Roboto-Light', 'sans-serif';
-        font-size: 42px;
-        text-align: center;
-        margin-top: 100px
+        font-size: 36px;
     }
-    .opinion {
-        font-size: 32px;
-        color: white;
-        font-family: 'Roboto-Light', sans-serif;
-    }
+}
 </style>
---
+++ src/components/Views/AboutUs/ReviewComponent.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="opinions">
+    <p class="review">{{ reviewText }}</p>
+    <p class="author">{{ authorName }}</p>
+</div>
+</template>
+
+  
+<script>
+export default {
+    props: {
+        reviewText: {
+            type: String,
+            required: true
+        },
+        authorName: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.opinions {
+    height: 250px;
+    width: 200px;
+    background-color: $secondary-color;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    color: $white;
+    overflow: hidden;
+}
+
+.review {
+    height: 50%;
+    text-align: center;
+    font-family: 'Roboto-Extra-Light', sans-serif;
+    padding: 0 20px;
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background-color: $primary-color;
+        border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: $scroll-track;
+        border-radius: 10px;
+    }
+}
+
+.author {
+    text-align: center;
+    font-family: 'Roboto-Light', sans-serif;
+}
+</style>
